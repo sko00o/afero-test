d17f371ced2265973859603a6c2ec4f1fe00b909
sko00o <lwylwy@outlook.com>
2022-06-16 17:18:05 +0800

    memmap: add test for mkdir

--- memmap/memmap_test.go
+++ memmap/memmap_test.go
@@ -13,6 +13,32 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestMemFsMkdirInConcurrency(t *testing.T) {
+	t.Parallel()
+	assert := require.New(t)
+
+	const dir = "test_dir"
+	const n = 1000
+
+	for i := 0; i < n; i++ {
+		fs := afero.NewMemMapFs()
+		c1 := make(chan error, 1)
+		c2 := make(chan error, 1)
+
+		go func() {
+			c1 <- fs.Mkdir(dir, 0755)
+		}()
+		go func() {
+			c2 <- fs.Mkdir(dir, 0755)
+		}()
+
+		// Only one attempt of creating the directory should succeed.
+		err1 := <-c1
+		err2 := <-c2
+		assert.NotEqualf(err1, err2, "run #%v, more than one success", i)
+	}
+}
+
 func TestListFilesAfterConcurrencyFileWrite(t *testing.T) {
 	const dir = "test_dir"
 	const n = 10000
